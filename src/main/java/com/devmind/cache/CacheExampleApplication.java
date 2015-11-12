3b4a7693622be4eb078498f4f4b021536025ee17
Guillaume EHRET <guillaume@dev-mind.fr>
2015-11-12 14:02:25 +0100

    Init Spring app

---
+++ src/main/java/com/devmind/cache/CacheExampleApplication.java
@@ -0,0 +1,14 @@
+package com.devmind.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GET <guillaume@dev-mind.fr>
+ */
+@SpringBootApplication
+public class CacheExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheExampleApplication.class, args);
+    }
+}
